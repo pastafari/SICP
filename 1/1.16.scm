dc7be93143114a592a0eca852db753e28e7a79bd
Mohit Thatte <mohit.thatte@gmail.com>
2011-07-09 20:58:44 +0530

    removed one variable from exponent-iter

--- 1/1.16.scm
+++ 1/1.16.scm
@@ -1,9 +1,9 @@
 (define (exp b n)
-  (define (exponent-iter a b n)
+  (define (exponent-iter a n)
     (cond ((= n 0) a)
 	  ((= n 1) (* a b))
-	  (else (exponent-iter (* a (* b b)) b (- n 2)))))
-  (exponent-iter 1 b n))
+	  (else (exponent-iter (* a (* b b)) (- n 2)))))
+  (exponent-iter 1 n))
 
 (exp 3 3)
 ;; Value 27
