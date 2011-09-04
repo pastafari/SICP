ff98a76e56f0b91f97dd0888538139132b1b0143
Mohit Thatte <mohit.thatte@gmail.com>
2011-09-04 18:33:52 +0530

    minor formatting change

--- 1/1.3.scm
+++ 1/1.3.scm
@@ -7,7 +7,7 @@
 ;;conditional to find largest 2 of 3 and return sum of squares
 (define (sum-of-squares-max-2-of-3 a b c) 
 	(cond ((and (<= a b) (<= a c)) (sum-of-squares b c))
-		  ((and (<= b a) (<= b c)) (sum-of-squares a c))
-		  ((and (<= c a) (<= c b)) (sum-of-squares a b))))
+	      ((and (<= b a) (<= b c)) (sum-of-squares a c))
+	      ((and (<= c a) (<= c b)) (sum-of-squares a b))))
 		
 (sum-of-squares-max-2-of-3 1 5 4)
